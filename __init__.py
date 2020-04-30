7ec503284e74cec08d66eccc89e3e7abf27dbde9
Felix Bosco <felixbosco@gmx.de>
2020-04-30 15:17:08 +0200

    Create __init__.py with plotting defaults

---
+++ __init__.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+
+# Define plot default settings
+plt.rc('figure', figsize=(11.69, 8.27))  # A4 format in inches
+plt.rc('xtick', direction='in')
+plt.rc('ytick', direction='in')
+plt.rc('font', family='serif', size=14)
